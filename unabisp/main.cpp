020b04d957c035742fce96dfb211fffc99d09c6b
fbenavDEV <francisco.benavides.murillo@una.cr>
2020-02-20 17:44:14 -0600

    uuu

--- unabisp/main.cpp
+++ unabisp/main.cpp
@@ -6,7 +6,7 @@
 
 int main()
 {
-	std::string s = "NODE[ASASA9ASA] {sadsadasa dsds} NODE[ssa]{sadsadsadsadsadsa}";
+	/**std::string s = "NODE[ASASA9ASA] {sadsadasa dsds} NODE[ssa]{sadsadsadsadsadsa}";
 	s.erase(std::remove(s.begin(), s.end(), ' '),s.end());
 	std::regex rx("NODE\\[[\\w]{1,}\\]\\{[\\w]{1,}\\}");
 	//NODE\\[[\\w]{1,}\\]
@@ -18,7 +18,7 @@ int main()
 	}
 	std::cout << "Codigo revisado";
 	std::string s = "0,2,3.5,3.9,4,3";	
-	std::regex rx("\\s*,\\s*");
+	std::regex rx("\\s*,\\s*");**/
 	const std::sregex_iterator end;
 	std::sregex_iterator sitr(std::cbegin(s), std::cend(s), rx);
 	std::smatch ss = *sitr;
